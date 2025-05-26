640625ce5053da610b4ae7836660e5ae75f497e1
Timo Blumer <timo.blumer@gewa.ch>
2025-05-26 14:07:38 +0200

    first commit

--- routes/vorstand.router.js
+++ routes/vorstand.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const vorstandController = require('../controller/vorstand.controller');
 
 // Öffentliche Route:
-router.get('/vorstand', vorstandController.getVorstand);
+router.get('/public', vorstandController.getVorstand);
 
 // Admin-Route zum Erstellen:
 router.post('/create', vorstandController.authenticateToken, vorstandController.createVorstand);
