73cceec89ce1d1f838cca74c1c8d371bb17e53fd
Timo Blumer <timo.blumer@gewa.ch>
2025-09-04 18:44:19 +0200

    Jlsfjslfj

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
+// Limits hochsetzen, damit große Base64-Bilder durchgehen
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
 
 const linksRouter = require('./routes/links.router');
 const adminRouter = require('./routes/admin.router');
@@ -25,9 +26,6 @@ const newsletterRouter = require('./routes/newsletter.router');
 const impressumRouter = require('./routes/impressum.router');
 const homeRouter = require('./routes/home.router');
 
-
-
-
 app.use('/api/links', linksRouter);
 app.use('/api/admin', adminRouter);
 app.use('/api/login', loginRouter);
@@ -37,8 +35,6 @@ app.use('/api/event', eventRouter);
 app.use('/api/newsletter', newsletterRouter);
 app.use('/api/impressum', impressumRouter);
 app.use('/api/home', homeRouter);
-
-
 
 // Fehlerbehandlung
 app.use((err, req, res, next) => {
@@ -51,4 +47,3 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server läuft auf Port ${PORT}....`);
 });
-
